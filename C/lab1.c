3210e251f2d95a574c7908a93e26bbb4ca12edb8
georgemore00 <81306021+georgemore00@users.noreply.github.com>
2023-01-31 23:27:10 +0100

    Update lab1.c
    funktioner

--- C/lab1.c
+++ C/lab1.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
+float update_height(float velocity, float height, int throttle) {
+    return height + velocity + (0.1 * throttle - 1.5) / 2;
+}
+
+float update_velocity(float velocity, int throttle) {
+    return velocity + (0.1 * throttle - 1.5);
+}
+
 int main(void)
 {
     int t=0, throttle;
     float velocity=-25, height=250;
     printf("Lunar decent challenge!\nYou will pilot a lunar decent the last 250m.\nEach turn represent 1-second decent time.\n");
     printf("Set the throttle for each turn (0-100)\nTime Height Velocity Throttle?\n");
-    printf("  0   250.0  -25.0   ");
 
     while(height>=0)
     {
+        printf("  %d   %.1f  %.1f   ", t, height, velocity);
         t+=1;
         scanf("%d", &throttle);
         if(throttle<0)
@@ -20,13 +28,13 @@ int main(void)
         {
             throttle = 100;
         }
-        height = height + velocity + (0.1*throttle-1.5)/2;
-        velocity = velocity + (0.1*throttle-1.5);
+        
+        height = update_height(velocity, height, throttle);
+        velocity = update_velocity(velocity, throttle);
         if(height<=0)
         {
             break;
         }
-        printf("  %d   %.1f  %.1f   ", t, height, velocity);
     }
 
     if(velocity<-2)
